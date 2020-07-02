aaca3f749c9c9d8e7aa244b5977cac20631f0870
harmony <61928705+harmonyzt@users.noreply.github.com>
2020-07-02 20:51:36 +0300

    News moved into plugin_init!

---
+++ dota_base/plugin_init.h
@@ -0,0 +1,15 @@
+new first_blood=0
+new name_killer[33]
+enum _:Massives
+{
+    exp, kills, gLevel, skillpoint
+};
+new UserData[50][Massives];
+new const levels[] =
+{
+	0,5,6,7,10,11,12,13,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,32,34,38,42,50,60,70
+};	//How much you need to get xp to next level
+enum _:Classes
+{
+    sniper
+};
